a9c4153c9ff2d10eeb5c34fd93e01662f3e29d2b
abhishek mishra <mishra.abhishek2808@gmail.com>
2025-11-09 13:33:19 +0530

    Fix API routes - add /api prefix to all endpoints

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -10,17 +10,17 @@ const api = axios.create({
 })
 
 export const submitQuery = async (query) => {
-  const response = await api.post('/query', { query })
+  const response = await api.post('/api/query', { query })
   return response.data
 }
 
 export const getJobStatus = async (jobId) => {
-  const response = await api.get(`/status/${jobId}`)
+  const response = await api.get(`/api/status/${jobId}`)
   return response.data
 }
 
 export const getJobResult = async (jobId) => {
-  const response = await api.get(`/result/${jobId}`)
+  const response = await api.get(`/api/result/${jobId}`)
   return response.data
 }
 
